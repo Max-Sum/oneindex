98cf6ffd2158c5d1645c8e917058bb868bb75a2a
Max Sum <maxsumprc@gmail.com>
2018-03-29 21:40:14 +0800

    Add onedrive_root to Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@ echo "<?php return array (
   'client_id' => '${ONEINDEX_CLIENT_ID}',
   'client_secret' => '${ONEINDEX_CLIENT_SECRET}',
   'redirect_uri' => '${ONEINDEX_REDIRECT_URI}',
+  'onedrive_root'=> '${ONEINDEX_DRIVE_ROOT}',
   'cache_type'=> '${ONEINDEX_CACHE_TYPE}',
   'cache_expire_time' => ${ONEINDEX_CACHE_EXPIRE_TIME},
   'cache_refresh_time' => ${ONEINDEX_CACHE_REFRESH_TIME},
