7187f14d357119b56d2bd03b158523ec3b0715a0
Max Sum <maxsumprc@gmail.com>
2018-03-29 20:20:33 +0800

    Sync configurations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@ echo "<?php return array (
   'client_id' => '${ONEINDEX_CLIENT_ID}',
   'client_secret' => '${ONEINDEX_CLIENT_SECRET}',
   'redirect_uri' => '${ONEINDEX_REDIRECT_URI}',
+  'cache_type'=> '${ONEINDEX_CACHE_TYPE}',
   'cache_expire_time' => ${ONEINDEX_CACHE_EXPIRE_TIME},
   'cache_refresh_time' => ${ONEINDEX_CACHE_REFRESH_TIME},
   'root_path' => '${ONEINDEX_ROOT_PATH}'
